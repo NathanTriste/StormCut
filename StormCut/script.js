154d1e75630e422c51a782218ce3242771de2dc0
Nathan <nathan.wolker@escola.pr.gov.br>
2026-05-20 21:56:26 -0300

    .

--- StormCut/script.js
+++ StormCut/script.js
@@ -1,4 +1,77 @@
 const ADMIN_NOME = "NATHAN";
+const ADMIN_SENHA = "PtsBf8uy";
+
+function criarConta(){
+
+    const nome = document.getElementById("cadastroNome").value;
+    const senha = document.getElementById("cadastroSenha").value;
+
+    if(nome === "" || senha === ""){
+        alert("Preencha tudo");
+        return;
+    }
+
+    localStorage.setItem("usuario", nome);
+    localStorage.setItem("senha", senha);
+
+    alert("Conta criada com sucesso!");
+
+    window.location.href = "index.html";
+}
+
+function login(){
+
+    const nome = document.getElementById("loginNome").value;
+    const senha = document.getElementById("loginSenha").value;
+
+    const usuarioSalvo = localStorage.getItem("usuario");
+    const senhaSalva = localStorage.getItem("senha");
+
+    if(nome === ADMIN_NOME && senha === ADMIN_SENHA){
+
+        localStorage.setItem("adminLogado", "true");
+
+        window.location.href = "home.html";
+
+        return;
+    }
+
+    if(nome === usuarioSalvo && senha === senhaSalva){
+
+        localStorage.setItem("usuarioLogado", "true");
+
+        window.location.href = "home.html";
+
+    }else{
+
+        alert("Nome ou senha incorretos");
+    }
+}
+
+function logout(){
+
+    localStorage.removeItem("usuarioLogado");
+    localStorage.removeItem("adminLogado");
+
+    window.location.href = "index.html";
+}
+
+function abrirAdmin(){
+
+    const admin = localStorage.getItem("adminLogado");
+
+    if(admin === "true"){
+
+        window.location.href = "admin.html";
+
+    }else{
+
+        alert("Apenas o admin pode acessar.");
+    }
+}
+
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
 function gerarCodigo(){
 
     return String(produtos.length + 1).padStart(4,'0');
